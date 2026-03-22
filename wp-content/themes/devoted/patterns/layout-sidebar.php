ffd6124a5a2000d2098de85351e0104907bc0c2d
Jenny Slaughter <wondermart@gmail.com>
2026-03-21 20:46:39 -0400

    Create sidebar pattern

---
+++ wp-content/themes/devoted/patterns/layout-sidebar.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Sidebar Layout
+ * Slug: devoted/layout-sidebar
+ */
+?>
+
+<!-- wp:columns -->
+    <div class="wp-block-columns alignwide has-global-padding is-style-sidebar">
+        <!-- wp:column -->
+        <div class="wp-block-column dvo-columns__main-wrapper">
+            <!-- wp:paragraph -->
+            <p>Main Column. Lorem ipsum dolor sit amet.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+        <!-- wp:column -->
+        <div class="wp-block-column dvo-columns__aside-wrapper">
+            <!-- wp:paragraph -->
+            <p>Sidebar Column. Lorem ipsum dolor sit amet.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+<!-- /wp:columns -->
